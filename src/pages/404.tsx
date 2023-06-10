a41d9e4c1b1f2422535d73290a98813c362b421e
josiaspiri <10800095+josiaspiri@users.noreply.github.com>
2023-06-10 12:26:56 -0400

    eslint

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react'
-import { Link, PageProps } from 'gatsby'
+import * as React from 'react';
+import {Link, PageProps} from 'gatsby';
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <span>
       Nothing is here. <Link to="/">Go back.</Link>
     </span>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
