5fda90afbf59d53d371df52e131181102e240595
josiaspiri <10800095+josiaspiri@users.noreply.github.com>
2023-06-14 13:32:11 -0400

    Leverage Head to add meta and more

---
+++ src/components/GlobalHead.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Head = () => (
+  <>
+    <title>Hello, World!</title>
+    <meta
+      name="description"
+      content="I don't know where I'm going but I'll know when I get there."
+    />
+    <body className="min-h-screen min-w-screen bg-black flex flex-col justify-center" />
+  </>
+);
+
+export default Head;
