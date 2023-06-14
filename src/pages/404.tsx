2da9cc4cefb57a1114d2cf061fd3734c95d30f16
josiaspiri <10800095+josiaspiri@users.noreply.github.com>
2023-06-14 13:36:11 -0400

    Update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,10 +3,14 @@ import {Link, PageProps} from 'gatsby';
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <span>
-      Nothing is here. <Link to="/">Go back.</Link>
-    </span>
+    <>
+      <h1>4 0 4 !</h1>
+      <p>
+        Nothing is here. <Link to="/">Go back.</Link>
+      </p>
+    </>
   );
 };
 
 export default NotFoundPage;
+export {Head} from '../components/GlobalHead';
