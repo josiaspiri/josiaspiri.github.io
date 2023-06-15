f6a8b7b7ade2966a4efbe87fe9a45ec1fcd47b34
josiaspiri <10800095+josiaspiri@users.noreply.github.com>
2023-06-14 22:19:53 -0400

    Addition of the best typeface ever

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import type {GatsbyBrowser} from 'gatsby';
 import Layout from './src/components/Layout';
+import '@fontsource/ibm-plex-sans';
 import './src/styles/global.scss';
 
 export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['IBM Plex Sans', 'sans-serif'],
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
